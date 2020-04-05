9f6dd2e285d5593176d4be0844d0f5e7f008c8c3
arashakn <arash.akn@gmail.com>
2020-04-04 17:18:31 -0700

    add gson

--- app/src/main/java/com/spin/models/Images.kt
+++ app/src/main/java/com/spin/models/Images.kt
@@ -1,10 +1,15 @@
 package com.spin.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Image(
+    @SerializedName("url")
     val url: String?,
+    @SerializedName("title")
     val title: String?
 )
 
 data class Images(
+    @SerializedName("data")
     val data: ArrayList<Image>
 )
